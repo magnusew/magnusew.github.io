eafc166f0fc97ebb163901a5727b8b187dfe67bd
Magnus Emil Wichmann <magnuswich@live.dk>
2024-03-01 17:55:36 +0100

    image test

--- scripts/script2.js
+++ scripts/script2.js
@@ -8,6 +8,9 @@ button.innerText = " Button # " + counter;
 button.className = "spamButton";
 menuListDiv.appendChild(button);
 
+let imageElement = document.createElement("img");
+let imgContainer = document.getElementById("imageContainer");
+
 
 button.addEventListener('click', function(event){
     counter++;
@@ -15,7 +18,16 @@ button.addEventListener('click', function(event){
     newButton.innerText = " Button # " + counter;
     newButton.className = "spamButton";
     menuListDiv.appendChild(newButton);
+    if(counter === 4)
+    {
+        imageElement.src = "/image/naughty.jpg";
+        /*document.body.style.background = "imageElement";*/
+
+    }
 });
+
+imgContainer.appendChild(imageElement);
+
 
 
 
